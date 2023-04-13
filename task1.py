90d4ea377f2da2686efae6041e0c416de1529e64
jkorn8 <josh.korngiebel@gmail.com>
2023-04-13 15:16:48 -0700

    added cryptography

--- task1.py
+++ task1.py
@@ -1,5 +1,10 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
 def main(filename):
-    print("Hello world")
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_EAX)
 
 
 if __name__ == "__main__":
