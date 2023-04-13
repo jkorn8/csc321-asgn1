9b0c81e23285173528f21d6d7a1cd893fbf50caa
jkorn8 <josh.korngiebel@gmail.com>
2023-04-13 16:24:02 -0700

    cbc done

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
 
-def main(filename):
+def ecb(filename):
     key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_ECB)
     cipher_text = b''
@@ -13,7 +13,6 @@ def main(filename):
             if not data:
                 break
             padded_data = add_padding(data)
-
             cipher_text += cipher.encrypt(padded_data)
     with open(filename[:-4] + "_encrypted" + filename[-4:], 'wb') as file_writer:
         file_writer.write(header)
@@ -26,5 +25,29 @@ def add_padding(data):
     return data
 
 
+def cbc(filename):
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipher_text = b''
+    iv = get_random_bytes(128)
+    chain_iv = bytearray(iv)
+    with open(filename, 'rb') as file:
+        header = file.read(54)
+        while True:
+            data = file.read(128)
+            padded_data = bytearray(add_padding(data))
+            if not data:
+                break
+            result = bytearray()
+            for i in range(len(padded_data)):
+                result.append(padded_data[i] ^ chain_iv[i])
+            chain_iv = result
+            cipher_text += cipher.encrypt(result)
+    with open(filename[:-4] + "_encrypted_cbc" + filename[-4:], 'wb') as file_writer:
+        file_writer.write(header)
+        file_writer.write(cipher_text)
+
+
 if __name__ == "__main__":
-    main("mustang.bmp")
+    ecb("mustang.bmp")
+    cbc("mustang.bmp")
