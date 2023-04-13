445eb11c6d16ffbf86b982609375f9d2871883c6
jkorn8 <josh.korngiebel@gmail.com>
2023-04-13 15:33:16 -0700

    started padding

--- task1.py
+++ task1.py
@@ -5,10 +5,13 @@ from Crypto.Random import get_random_bytes
 def main(filename):
     with open(filename, 'rb') as file:
         data = file.read(128)
-        value = int.from_bytes(data, byteorder='big', signed=False)
-        print(hex(value))
+        print(len(data))
+        ascii_text = data.decode('ascii')
+        print(ascii_text)
         key = get_random_bytes(16)
         cipher = AES.new(key, AES.MODE_ECB)
+        cipher_text = cipher.encrypt(data)
+        print(cipher_text)
 
 
 if __name__ == "__main__":
