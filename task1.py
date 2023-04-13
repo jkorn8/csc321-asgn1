d533327def78d65f1cb013f8f4bf34ee80444c16
jkorn8 <josh.korngiebel@gmail.com>
2023-04-13 15:23:07 -0700

    binary

--- task1.py
+++ task1.py
@@ -3,8 +3,12 @@ from Crypto.Random import get_random_bytes
 
 
 def main(filename):
-    key = get_random_bytes(16)
-    cipher = AES.new(key, AES.MODE_EAX)
+    with open(filename, 'rb') as file:
+        data = file.read(128)
+        value = int.from_bytes(data, byteorder='big', signed=False)
+        print(hex(value))
+        key = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_ECB)
 
 
 if __name__ == "__main__":
