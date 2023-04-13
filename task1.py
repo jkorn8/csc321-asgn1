a9542710615aaa821e638c1d0c7303106bc34c79
jkorn8 <josh.korngiebel@gmail.com>
2023-04-13 15:39:51 -0700

    works with whole file now

--- task1.py
+++ task1.py
@@ -3,15 +3,20 @@ from Crypto.Random import get_random_bytes
 
 
 def main(filename):
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_ECB)
+    cipher_text = b''
     with open(filename, 'rb') as file:
-        data = file.read(128)
-        print(len(data))
-        ascii_text = data.decode('ascii')
-        print(ascii_text)
-        key = get_random_bytes(16)
-        cipher = AES.new(key, AES.MODE_ECB)
-        cipher_text = cipher.encrypt(data)
-        print(cipher_text)
+        while True:
+            data = file.read(128)
+            if not data:
+                break
+            print(len(data))
+            ascii_text = data.decode('ascii')
+            print(ascii_text)
+            cipher_text += cipher.encrypt(data)
+    print(cipher_text)
+
 
 
 if __name__ == "__main__":
