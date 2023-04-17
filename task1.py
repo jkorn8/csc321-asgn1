40f1ee6f81b2b466b1da408243e1585a9a5c4acf
jkorn8 <josh.korngiebel@gmail.com>
2023-04-16 18:25:55 -0700

    changed some files for task 2

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
 
-def ecb(filename):
+def ecb(filename, cipher):
     key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_ECB)
     cipher_text = b''
@@ -19,17 +19,8 @@ def ecb(filename):
         file_writer.write(cipher_text)
 
 
-def add_padding(data):
-    missing_len = 128 - len(data)
-    data = data + bytes(missing_len * chr(missing_len), 'utf-8')
-    return data
-
-
-def cbc(filename):
-    key = get_random_bytes(16)
-    cipher = AES.new(key, AES.MODE_ECB)
+def cbc(filename, cipher, iv):
     cipher_text = b''
-    iv = get_random_bytes(128)
     chain_iv = bytearray(iv)
     with open(filename, 'rb') as file:
         header = file.read(54)
@@ -48,6 +39,15 @@ def cbc(filename):
         file_writer.write(cipher_text)
 
 
+def add_padding(data):
+    missing_len = 128 - len(data)
+    data = data + bytes(missing_len * chr(missing_len), 'utf-8')
+    return data
+
+
 if __name__ == "__main__":
-    ecb("mustang.bmp")
-    cbc("mustang.bmp")
+    key = get_random_bytes(16)
+    my_cipher = AES.new(key, AES.MODE_ECB)
+    my_iv = get_random_bytes(128)
+    ecb("mustang.bmp", my_cipher)
+    cbc("mustang.bmp", my_cipher, my_iv)
